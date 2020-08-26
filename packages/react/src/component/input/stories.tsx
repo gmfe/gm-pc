d5a567895ae0779c188f4b07be6962a389c83358
liyatang <liyatang@qq.com>
2020-08-26 11:28:46 +0800

    WIP: input

---
+++ packages/react/src/component/input/stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Input from './input'
+import { observable } from 'mobx'
+
+const store = observable({
+  value: '',
+})
+
+export const ComInput = () => {
+  return (
+    <div className='gm-padding-10'>
+      <Input
+        placeholder='请输入'
+        value={store.value}
+        onChange={(e) => {
+          store.value = e.target.value
+        }}
+      />
+    </div>
+  )
+}
+
+export default {
+  title: '表单/Input',
+}
