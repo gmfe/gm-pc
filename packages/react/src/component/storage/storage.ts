d2ceb89de7cd1a4aa298e16dfa10ab4cd0b475ca
liyatang <liyatang@qq.com>
2020-10-31 16:06:31 +0800

    fix: storage prefix

--- packages/react/src/component/storage/storage.ts
+++ packages/react/src/component/storage/storage.ts
@@ -1,7 +1,7 @@
 import { StorageFactory } from '@gm-common/tool'
 
-const LocalStorage = new StorageFactory('_gm-mobile_', window.localStorage)
-const SessionStorage = new StorageFactory('_gm-mobile_', window.sessionStorage)
+const LocalStorage = new StorageFactory('_gm-pc_', window.localStorage)
+const SessionStorage = new StorageFactory('_gm-pc_', window.sessionStorage)
 
 export { SessionStorage, LocalStorage }
 export default LocalStorage
