2842702726f108d1a1dcab30b2269d5a4b2caf5d
liyatang <liyatang@qq.com>
2020-11-11 18:04:52 +0800

    fix: pagination limit

--- packages/react/src/component/pagination/left.tsx
+++ packages/react/src/component/pagination/left.tsx
@@ -2,12 +2,18 @@ import React, { FC } from 'react'
 import { Select } from '../select'
 import { Flex } from '../flex'
 import { PaginationProps } from './types'
+import _ from 'lodash'
 
-const limitData = [
-  { value: 10, text: '10' },
-  { value: 20, text: '20' },
-  { value: 50, text: '50' },
-]
+function getLimitData(limit: number) {
+  const limitData = [
+    { value: limit, text: limit + '' },
+    { value: 10, text: '10' },
+    { value: 20, text: '20' },
+    { value: 50, text: '50' },
+  ]
+
+  return _.uniqBy(limitData, (v) => v.value)
+}
 
 const Left: FC<PaginationProps> = ({ paging, onChange }) => {
   const { need_count, count, limit } = paging
@@ -26,7 +32,7 @@ const Left: FC<PaginationProps> = ({ paging, onChange }) => {
       {need_count && <span>共{count}条记录,&nbsp;</span>}
       <span>每页&nbsp;</span>
       <Select
-        data={limitData}
+        data={getLimitData(limit)}
         value={limit}
         onChange={handleChange}
         style={{ width: '60px' }}
