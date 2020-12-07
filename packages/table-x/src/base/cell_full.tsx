01f93b546b437a46646327ae34345fd91c1a99bb
michwh <fjsxhwh@163.com>
2020-12-07 17:13:21 +0800

    fix: tableCellFullItem可传style

--- packages/table-x/src/base/cell_full.tsx
+++ packages/table-x/src/base/cell_full.tsx
@@ -1,11 +1,18 @@
-import React, { FC } from 'react'
+import React, { FC, CSSProperties } from 'react'
 
 const TableXCellFull: FC = ({ children }) => {
   return <div className='gm-table-x-cell-full'>{children}</div>
 }
+interface TableXCellFullItemProps {
+  style: CSSProperties
+}
 
-const TableXCellFullItem: FC = ({ children }) => {
-  return <div className='gm-table-x-cell-full-item'>{children}</div>
+const TableXCellFullItem: FC<TableXCellFullItemProps> = ({ children, style }) => {
+  return (
+    <div className='gm-table-x-cell-full-item' style={style}>
+      {children}
+    </div>
+  )
 }
 
 export { TableXCellFull, TableXCellFullItem }
