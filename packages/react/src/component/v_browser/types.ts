bfc185ca139234b6a7accaf5f6762a2d982f3ba5
jawei <1005945485@qq.com>
2022-07-05 15:39:09 +0800

    doc: update v_browser stories

--- packages/react/src/component/v_browser/types.ts
+++ packages/react/src/component/v_browser/types.ts
@@ -14,7 +14,7 @@ export interface VBrowserWindow {
 export interface VBrowserProps {
   /** 窗口数量限制 */
   maxLength?: number
-  /**  */
+  /** 准备就绪 */
   onReady?: Function
   /** 重新打开vBrowser时恢复已打开窗口, 默认为true */
   restore?: boolean
@@ -26,7 +26,12 @@ export interface VBrowserProps {
   ) => void
   /** 窗口打开/切换前调用，返回false会阻止窗口打开/切换 */
   auth?: (from?: VBrowserWindow, to?: VBrowserWindow) => Promise<boolean> | boolean
+  /**
+   * 错误码参考
+   * code: 0, message: '超过最大允许的窗口数量'
+   */
   onError?: (error: { code: number; message: string }) => void
+  /** 打开窗口如果没有传入标题，则使用此方法取标题 */
   autoTitle?: (path: string) => string
 }
 
