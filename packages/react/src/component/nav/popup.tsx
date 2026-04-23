22dbb1109ec89d5f6ed0c530d2f51797a5fde89f
hufeiyu <hufeiyu@guanmai.cn>
2026-04-23 12:18:21 +0800

    refactor(nav): 采用 CSS Grid 替换弹出菜单布局
    - 移除弹出菜单最大宽度和宽度计算函数
    - 用 gm-nav-popup-grid 网格容器替代 Flex 布局实现多列排列
    - 调整样式使用 grid-template-columns 生成 5 列网格
    - 设置列间距和行间距为 24px
    - 优化二级菜单标题及布局间距实现更灵活的自适应显示

--- packages/react/src/component/nav/popup.tsx
+++ packages/react/src/component/nav/popup.tsx
@@ -1,7 +1,6 @@
 import React, { FC, MouseEventHandler, useLayoutEffect, useRef, useState } from 'react'
 import { PopupProps } from './types'
 import classNames from 'classnames'
-import { Flex } from '../flex'
 import A from './a'
 
 const Popup: FC<PopupProps> = ({
@@ -22,20 +21,6 @@ const Popup: FC<PopupProps> = ({
     }
   }, [parentRect])
 
-  /**
-   * 在 对应的 div 设置了 最大宽度 900 px
-   * @description UI 图是超过五个换行, 现在无论多少模块都在一行, 根据UI图对宽度进行处理
-   * @param number 二级菜单的个数
-   * @returns popup 的宽度
-   */
-  const setPopupWidth = (number: number): string => {
-    // 只有一个的基本宽度, 根据 UI图, 一个三级路由占据的 宽度 + 左右margin
-    const baseWidth = 212
-    // 有多个模块 半句的最大宽段, 超出 900 换行
-    const maxWidth = baseWidth + (number - 1) * 172
-    return maxWidth + 'px'
-  }
-
   const handleMouseEnter: MouseEventHandler<HTMLDivElement> = (e) => {
     e.currentTarget.classList.add('gm-nav-three-wrap-bg')
   }
@@ -51,11 +36,9 @@ const Popup: FC<PopupProps> = ({
       style={{
         marginTop: `${marginTop}px`,
         top: parentRect.top,
-        width: `${setPopupWidth(data.length)}`,
-        maxWidth: '900px',
       }}
     >
-      <Flex wrap>
+      <div className='gm-nav-popup-grid'>
         {data.map((v, i) => (
           <div key={i} className='gm-nav-two' style={v.style}>
             {!!v.name && <div className='gm-nav-two-title'>{v.name}</div>}
@@ -92,7 +75,7 @@ const Popup: FC<PopupProps> = ({
             </div>
           </div>
         ))}
-      </Flex>
+      </div>
     </div>
   )
 }
